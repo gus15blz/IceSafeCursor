2764cd57b415f71ddfd442fc52477bdc21ba2a6c
gus15blz <gustavo.buenocastilho620@gmail.com>
2025-05-14 15:12:49 -0300

    atualização do projeto

--- src/pages/Sales/index.jsx
+++ src/pages/Sales/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import  { useState, useEffect } from 'react'
 import api from '../../services/api'
 
 function Sales() {
@@ -56,3 +56,5 @@ function Sales() {
 }
 
 export default Sales 
+
+//teste
